5060c3bf9b301ee02d9a4a6ba2dfd2f54cc6c9fe
Seth Daetwiler <sericha@rei.com>
2022-07-09 17:32:44 -0700

    Active capture and update of sound magnitude

--- ARSoundFinder/ARSoundFinder/ViewController.swift
+++ ARSoundFinder/ARSoundFinder/ViewController.swift
@@ -7,27 +7,58 @@
 
 import UIKit
 import RealityKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    
+    @IBOutlet weak var arView: ARView!
+    @IBOutlet weak var levelLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
+        levelLabel.textColor = .red
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
         
         guard let session = setupMicrophone() else {
-            print("error setting up session in main")
+            print("Error setting up session in main")
             return
         }
         
-        session.requestRecordPermission { response in
-            print(response)
+        startAudioCapture()
+        
+    }
+    
+    // TODO: Find a home for capture related code
+    func startAudioCapture(){
+        let docPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let sampleFileName = docPath.appendingPathComponent("rec.m4a")
+        let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                      AVSampleRateKey: 12000,
+                      AVNumberOfChannelsKey: 1,
+                      AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+        
+        do {
+            let sessionRecorder = try AVAudioRecorder(url: sampleFileName, settings: settings)
+            sessionRecorder.record()
+            sessionRecorder.isMeteringEnabled = true
+            
+            // Contains all audio capture refresh actions
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                sessionRecorder.updateMeters()
+                let db = sessionRecorder.averagePower(forChannel: 0)
+                
+                self.levelLabel.text = "\(Int(db))"
+                print(db)
+            }
+            
+        } catch {
+            print("failed to record")
         }
     }
 }
